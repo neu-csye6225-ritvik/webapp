f340025b97004233e572cd74cfe3a966670239ac
Ritvik <paramkusham.s@northeastern.edu>
2023-10-24 15:15:56 -0400

    webappuser setup and system d with user 2

--- systemD.sh
+++ systemD.sh
@@ -40,7 +40,7 @@ echo "|                                                             |"
 echo "+-------------------------------------------------------------+"
 
 cd 
-sudo systemctl start webappuser
-sudo systemctl status webappuser
-sudo systemctl enable webappuser
+sudo systemctl start webapp
+sudo systemctl status webapp
+sudo systemctl enable webapp
 

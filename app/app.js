3f5ef260f836ba22abc2b475f2c151af3e2bd93a
Ritvik <paramkusham.s@northeastern.edu>
2023-11-04 16:21:27 -0400

    statsd setup

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const route =  require("./route/index.js");
+const statsdMiddleware = require('./middleware/stasd-middle.js');
 
 const fs =require('fs');
 
@@ -10,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(statsdMiddleware.reqCounter);
+app.use(statsdMiddleware.reqTimer);
+app.use(statsdMiddleware.responseCodeCounter);
 
 route(app);
 

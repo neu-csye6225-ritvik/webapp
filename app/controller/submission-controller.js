730b94f6363b740cd316e729717ddb8390cc8e39
Ritvik <114260124+ritvikneu@users.noreply.github.com>
2023-12-04 17:14:39 -0500

    Assign10 7 (#110)
    * submission
    
    * submission 2
    
    * sub controller omit aws cred
    
    * submission, user, assign controller and webapp validations
    
    * crud assignment changes, users csv and webapp validations
    
    * crud assignment changes, users csv and webapp validations
    
    * CD workflow, update launch template, asg refresh
    
    * CD workflow, update launch template, asg refresh
    
    * CD workflow
    
    * CD workflow 2
    
    * CD workflow 3
    
    * CD workflow 4
    
    * CD workflow 5
    
    * CD workflow 6
    
    * cd testing

--- app/controller/submission-controller.js
+++ app/controller/submission-controller.js
@@ -105,7 +105,6 @@ submissionController.createSubmission = async (req, res) => {
         }
 
 
-
         const newSubmission = await Submission.create({
             assignment_id: assignment_id,
             user_id: id,
